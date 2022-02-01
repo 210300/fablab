663a505ecea4027d8f1c4dc6c234140b6b36ebb1
karma Tshewang <karmatshew471@gmail.com>
2022-02-01 13:50:51 +0530

    make it better

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const flash = require('connect-flash');
 const dotenv = require("dotenv");
 dotenv.config({ path: './.env'});
 
+const port = process.env.PORT || 5000
 
 const app = express();
 app.use(express.json());
@@ -76,6 +77,4 @@ app.use('/auth', require('./routes/auth'));
 
 
 app.use(ignoreFavicon);
-app.listen(3000,'0.0.0.0', ()=>{
-  console.log("Server started on port: "+3000);
-});
+app.listen(port);
